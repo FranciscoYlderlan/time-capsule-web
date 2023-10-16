6ee915144676a6c1387b9e8e55f35e3edbce6007
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-10-16 16:56:45 -0300

    :sparkles: feat: Created initial section content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <main className="grid grid-cols-2 min-h-screen">
+      <section></section>
+      <section className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a {' '} 
+            <a className="underline hover:text-gray-50 transition-colors" href="" >criar agora</a>!
+          </p>
+        </div>
+      </section>
+    </main>
   )
 }
